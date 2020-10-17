b9353a582b46c5f88bc1f26a66b6f2906f08d298
nehashettar <shettarneha92@gmail.com>
2020-10-17 14:33:54 +0530

    Added
    Child and Grand Child Component
    BootStarp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { GrandChildComponent } from './grand-child/grand-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    GrandChildComponent,
+
   ],
   imports: [
     BrowserModule
